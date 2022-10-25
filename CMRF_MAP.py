02814f7e6f3e0b5b8c234faef12cf2b2b2ed8d37
JYKIM7 <71793706+JYKIM7@users.noreply.github.com>
2022-10-25 10:50:47 -0400

    Update CMRF_MAP.py

--- CMRF_MAP.py
+++ CMRF_MAP.py
@@ -17,40 +17,49 @@ def map_fun(b_0, x_design, y_design, dx, template, beta, mu, s2, gamma, nclasses
             u1 = zeros(num_vox, nclasses)
             u2 = zeros(num_vox, nclasses)
             for ll in range(nclasses):
-                temp_i = (r_i - mu_i[ll])**2/s2_i/2+np.log(s2_i)/2
+                temp_i = np.log( (r_i - np.repeat( np.mean(mu_i) , r_i.size ) )**2/s2_i/2 ) + np.log(s2_i)/2
                 u1[:, ll] = u1[:, l]+temp_i
-                b0_i.shape
-                ind2_1.shape
+                # b0_i.shape
+                # ind2_1.shape
                 for vii in range(num_vox):
                     u3 = np.zeros(num_vox)
                     if len(template.shape) == 2:
                         vec_template = np.reshape(template, (template.shape[0] * template.shape[1], 1))
-                        ind_list = np.where(vec_template == 1)
-                        ind = ind_list[vii]
-                        sub2 = ind2sub(template.shape, ind, 2)
-                        ind2_1 = sub2ind(template.shape, [sub2[0] + 1, sub[1]])
-                        ind2_2 = sub2ind(template.shape, [sub2[0], sub2[1] + 1])
-                        ind2_3 = sub2ind(template.shape, [sub2[0] - 1, sub2[1]])
-                        ind2_4 = sub2ind(template.shape, [sub2[0], sub2[1] - 1])
-                        u3[vii] = delta_fun(ll, b0_i[ind2_1]) + delta_fun(ll, b0_i[ind2_2])\
-                             + delta_fun(ll, b0_i[ind2_3]) + delta_fun(ll, b0_i[ind2_4])
-                    else:
-                        vec_template = np.reshape(template, (template.shape[0] * template.shape[1]
-                                                             * template.shape[2], 1))
-                        ind_list = np.where(vec_template == 1)
-                        ind = ind_list[vii]
-                        sub3 = ind2sub(template.shape, ind, 3)
-                        ind3_1 = sub2ind(template.shape, [sub3[0] + 1, sub3[1], sub3[2]])
-                        ind3_2 = sub2ind(template.shape, [sub3[0], sub3[1] + 1, sub3[2]])
-                        ind3_3 = sub2ind(template.shape, [sub3[0], sub3[1], sub3[2] + 1])
-                        ind3_4 = sub2ind(template.shape, [sub3[0] - 1, sub3[1], sub3[2]])
-                        ind3_5 = sub2ind(template.shape, [sub3[0], sub3[1] - 1, sub3[2]])
-                        ind3_6 = sub2ind(template.shape, [sub3[0], sub3[1], sub3[2] - 1])
-                        u3[vii] = delta_fun(ll, b0_i[ind3_1]) + delta_fun(ll, b0_i[ind3_2]) + \
-                             delta_fun(ll, b0_i[ind3_3]) + delta_fun(ll, b0_i[ind3_4]) \
-                             + delta_fun(ll, b0_i[ind3_5]) + delta_fun(ll, b0_i[ind3_6])
-                    u2[vii, ll] = np.sum(u3)
-            u = u1 + u2*gamma
+                    ind_list = np.where(vec_template == 1)[0]
+                
+                    ind = ind_list[vii]
+                    sub2 = ind2sub_v2(template.shape, np.array(ind))
+                    ind2_1 = sub2ind_v2(template.shape, [sub2[0] + 1, sub2[1]])
+                    ind2_2 = sub2ind_v2(template.shape, [sub2[0], sub2[1] + 1])
+                    ind2_3 = sub2ind_v2(template.shape, [sub2[0] - 1, sub2[1]])
+                    ind2_4 = sub2ind_v2(template.shape, [sub2[0], sub2[1] - 1])
+                    
+                    b_0_new = pd.DataFrame(b0_i.T, index=ind_list)
+                    b_0_new.columns = ['value']
+                    
+                    if ind2_1 in ind_list: 
+                        u3_1 = delta_fun(ll, b_0_new.loc[ind2_1].value.astype(int))
+                    else:   
+                        u3_1 = delta_fun(ll, 0)
+                    if ind2_2 in ind_list: 
+                        u3_2 = delta_fun(ll, b_0_new.loc[ind2_2].value.astype(int))
+                    else:   
+                        u3_2 = delta_fun(ll, 0)
+                    if ind2_3 in ind_list: 
+                        u3_3 = delta_fun(ll, b_0_new.loc[ind2_3].value.astype(int))
+                    else:   
+                        u3_3 = delta_fun(ll, 0)
+                    if ind2_4 in ind_list: 
+                        u3_4 = delta_fun(ll, b_0_new.loc[ind2_4].value.astype(int))
+                    else:   
+                        u3_4 = delta_fun(ll, 0)
+                    
+                    u3[vii] = -(u3_1 + u3_2 + u3_3 + u3_4)
+                
+                #else:
+                u2[vii, ll] = np.sum(u3)   
+                
+        u = u1 + u2*gamma
             b0_i = np.argmin(u, axis=1)-1
 
         if len(template.shape) == 2:
@@ -67,5 +76,5 @@ def map_fun(b_0, x_design, y_design, dx, template, beta, mu, s2, gamma, nclasses
         r_i1 = r_i[b0_i == 1]
         mu[ii, 1] = np.sum(u_i1 * r_i1) / np.sum(u_i1)
 
-    """return b_0, mu"""
-    return b_0, mu
+    """return b_0"""
+    return b_0
