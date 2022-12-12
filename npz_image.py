93ef53cd1374adc1344f334fffa234dccdc62d94
JYKIM7 <71793706+JYKIM7@users.noreply.github.com>
2022-12-12 07:55:19 -0500

    Update npz_image.py

--- npz_image.py
+++ npz_image.py
@@ -4,7 +4,7 @@ import glob
 import pickle
 import pandas as pd
 import os
-input_dir = 'C:/Users/JYKIM/Desktop/Student_Kim_220915/DADP/input/thickness_map2'
+input_dir = '~~~'
 os.chdir(input_dir)
 
 ######################################################
@@ -52,128 +52,116 @@ dt2_1 = dt2_1[idxx.argsort(),:,:]
 ######################################################
 ### Make template file (=mask)
 ######################################################
+def template_fun(data):
+    temp=np.ones(shape=(data.shape[1],data.shape[2]) )
+    for i in range(data.shape[1]):
+        for j in range(data.shape[2]):
+            if np.sum(data[:,i,j]) < 1:
+                temp[i][j] = 0
+            else:
+                temp[i][j] = 1
+    return temp
 
-temp=np.ones(shape=(dt1_1_del.shape[1],dt1_1_del.shape[2]) )
-temp
-for i in range(dt1_1.shape[1]):
-    for j in range(dt1_1.shape[1]):
-        if np.sum(dt1_1[:,i,j]) < 1:
-            temp[i][j] = 0
-        else:
-            temp[i][j] = 1
-mask1 = temp.astype(int)
+mask1 = template_fun(dt1_1_del)
 plt.imshow(mask1)
 
-temp=np.ones(shape=(dt2_1.shape[1],dt2_1.shape[2]) )
-temp
-for i in range(dt2_1.shape[1]):
-    for j in range(dt2_1.shape[1]):
-        if np.sum(dt2_1[:,i,j]) < 1:
-            temp[i][j] = 0
-        else:
-            temp[i][j] = 1
-mask2 = temp.astype(int)
+mask2 = template_fun(dt2_1)
 plt.imshow(mask2)
 
+
 ######################################################
-### NMF
 ######################################################
 
-# Construct the average disease map for each subject
-df = pd.read_pickle('thickness_results.pkl')
-df_femoral = df[df['cartilage_type']=='femoral']
-sub_id_unique = np.unique(df_femoral['patient_id'])
-m = len(sub_id_unique)
-m_v = mask1.shape[0]*mask1.shape[1]
-A = np.zeros( shape=(m_v, m) )
+def full_to_mask(dt, template):
+    vec_template = np.reshape(template, (template.shape[0] * template.shape[1], 1))
+    ind_list = np.where(vec_template == 1)[0]
+    df_maskinfo = pd.DataFrame({'value': dt.flatten()[ind_list] , 'index': ind_list} )
+    return df_maskinfo
 
-aa=0
-for ll in range(m):
-    time_len = len(df_femoral[df_femoral['patient_id'] == sub_id_unique[ll]]['cartilage_type_id'])
-    d_map_mean = np.mean( dt2_1[range(aa, aa + time_len -1),:,:] , axis=0)
-    # plt.imshow( d_map_mean )
-    A[:,ll] = d_map_mean.flatten()
-    aa = aa + 1
+def mask_to_full(dt_value, dt_index, template):
+    ddd = np.zeros(shape=(template.shape[0] * template.shape[1] , 1))
+    ddd[dt_index] = np.reshape(np.array(dt_value), (len(dt_value), 1))
+    full_array = np.reshape(ddd, (template.shape[0],  template.shape[1]), order='a')
+    return full_array
 
-plt.imshow(np.reshape(A[:,311], (mask1.shape[0],  mask1.shape[1]) ) )
-
-from sklearn.decomposition import NMF
-K = 12  # number of clusters
-model = NMF(n_components = K, init='random')
-AA = A
-W = model.fit_transform(AA)
-H = model.components_ # W.shape, H.shape
-new_img = np.dot(W,H[:,311]) # new_img.shape
-plt.imshow(np.reshape(new_img, (mask1.shape[0],  mask1.shape[1]) ) )
-# plt.imshow(np.reshape(new_img, (mask1.shape[0],  mask1.shape[1]) ).astype(int))
-
-
+dt = dt1_1_del[1,:,:]
 template = mask1
-vec_template = np.reshape(template, (template.shape[0] * template.shape[1], 1))
-ind_list = np.where(vec_template == 1)[0]
-ddd = np.zeros(shape=(template.shape[0] * template.shape[1] , 1))
-bbb = pd.DataFrame({'value': new_img.flatten()[ind_list]}, index=ind_list)
+full_to_mask(dt, template)
 
-k = 5
-quantiles = pd.qcut(bbb['value'], k, labels=False).astype(int) + 1
-bbb = bbb.assign(quantile = quantiles.values) 
-ddd[ind_list] = np.reshape(np.array(bbb['quantile']), (len(ind_list), 1))
-eee = np.reshape(ddd, (template.shape[0],  template.shape[1]), order='a')
-plt.imshow(eee)
+d_value = full_to_mask(dt, template)['value']
+d_index = full_to_mask(dt, template)['index']
+plt.imshow( mask_to_full(d_value, d_index, template) )
 
-quantiles = pd.qcut(bbb['value'], k, labels=False).astype(int) + 1
-bbb = bbb.assign(quantile = quantiles.values)
-bbb = bbb.replace(np.unique(bbb['quantile'].values)[:-1], 1)
-ddd[ind_list] = np.reshape(np.array(bbb['quantile']), (len(ind_list), 1))
-eee = np.reshape(ddd, (template.shape[0],  template.shape[1]), order='a')
-plt.imshow(eee)
-
+#############################################################
 # Construct the average disease map for each subject
 df = pd.read_pickle('thickness_results.pkl')
-df_tibial = df[df['cartilage_type']=='tibial']
-sub_id_unique = np.unique(df_tibial['patient_id'])
-m = len(sub_id_unique)
-m_v = mask1.shape[0]*mask1.shape[1]
-A = np.zeros( shape=(m_v, m) )
+df = df[df['cartilage_type']=='femoral']
+dt = dt1_1_del
 
-aa=0
+dtt = np.zeros(shape = dt.shape) 
+temp = template_fun(dt1_1_del)
+np.unique(temp.flatten())
+ii=0
+for ii in range(dt.shape[0]):
+    tem = temp
+    idx = np.where(dt[ii,:,:] > np.quantile(dt[ii,:,:], 0.8))
+    tem[idx] = 1
+    idx2 = np.where(dt[ii,:,:] > np.quantile(dt[ii,:,:], 0.9))
+    tem[idx2] = 2
+    dtt[ii,:,::] = tem
+    # plt.imshow(temp) 
+    print(ii)
+
+# dtt = dt
+
+# np.savez('C:/Users/JYKIM/Desktop/dtt.npz', dtt)
+# np.load('C:/Users/JYKIM/Desktop/dtt.npz')
+# abc = np.load('C:/Users/JYKIM/Desktop/dtt.npz')
+# dtt = abc['arr_0']
+np.unique(dtt.flatten())
+plt.imshow(dtt[0,:,:]) 
+
+
+
+mask1 = template_fun(dt1_1_del)
+template = mask1
+sub_id_unique = np.unique(df['patient_id'])
+m = len(sub_id_unique)
+m_v = len(np.where(template.flatten() == 1)[0])
+A = np.zeros( shape=(m_v, m) ) # A.shape
+
+aa = 0
 for ll in range(m):
-    time_len = len(df_tibial[df_tibial['patient_id'] == sub_id_unique[ll]]['cartilage_type_id'])
-    # d_map_mean = np.mean( dt2_1[range(aa, aa + time_len -1),:,:] , axis=0)
-    d_map_mean = np.mean( dt2_1[range(aa, aa + time_len -1),:,:] , axis=0)
-    # plt.imshow( d_map_mean )
-    A[:,ll] = d_map_mean.flatten()
+    time_len = len(df[df['patient_id'] == sub_id_unique[ll]]['cartilage_type_id'])
+    d_map_mean = np.mean( dtt[range(aa, aa + time_len -1),:,:] , axis=0)
+    A[:, ll] = full_to_mask(d_map_mean, template)['value'] # d_map_mean.shape
     aa = aa + 1
 
-plt.imshow(np.reshape(A[:,311], (mask1.shape[0],  mask1.shape[1]) ) )
+plt.imshow( mask_to_full(A[:,1], np.where(template.flatten() == 1)[0], template) )
+# plt.imshow( dt[0,:,:])
+
 
 from sklearn.decomposition import NMF
-K = 12  # number of clusters
-model = NMF(n_components = K, init='random')
-AA = A
-W = model.fit_transform(AA)
-H = model.components_ # W.shape, H.shape
-new_img = np.dot(W,H[:,311]) # new_img.shape
-plt.imshow(np.reshape(new_img, (mask2.shape[0],  mask2.shape[1]) ) )
-# plt.imshow(np.reshape(new_img, (mask1.shape[0],  mask1.shape[1]) ).astype(int))
+K = 3 # number of clusters
+model = NMF(n_components = K,max_iter=300)
+AA = A - 1
+W = model.fit_transform(AA) # (60795, 3)
+H = model.components_       # (3, 351)
 
 
-template = mask2
-vec_template = np.reshape(template, (template.shape[0] * template.shape[1], 1))
-ind_list = np.where(vec_template == 1)[0]
-ddd = np.zeros(shape=(template.shape[0] * template.shape[1] , 1))
-bbb = pd.DataFrame({'value': new_img.flatten()[ind_list]}, index=ind_list)
+I = np.argsort(H, axis=0) # I.shape
+label = I[I.shape[0]-1,:] # same as H.argmax(axis=0) 
 
-k = 6
-quantiles = pd.qcut(bbb['value'], k, labels=False).astype(int) + 1
-bbb = bbb.assign(quantile = quantiles.values) 
-ddd[ind_list] = np.reshape(np.array(bbb['quantile']), (len(ind_list), 1))
-eee = np.reshape(ddd, (template.shape[0],  template.shape[1]), order='a')
-plt.imshow(eee)
 
-quantiles = pd.qcut(bbb['value'], k, labels=False).astype(int) + 1
-bbb = bbb.assign(quantile = quantiles.values)
-bbb = bbb.replace(np.unique(bbb['quantile'].values)[:-1], 1)
-ddd[ind_list] = np.reshape(np.array(bbb['quantile']), (len(ind_list), 1))
-eee = np.reshape(ddd, (template.shape[0],  template.shape[1]), order='a')
-plt.imshow(eee)
+for ii in range(np.size(H, 0)):
+	idk = np.where(label==ii)
+    # roi = np.zeros(shape=(310,310))
+	globals()["roi_{}".format(ii)] = W[:,ii]*np.mean(H[ii,idk])
+
+
+plt.imshow( mask_to_full(roi_0, np.where(template.flatten() == 1)[0], template) )
+plt.imshow( mask_to_full(roi_1, np.where(template.flatten() == 1)[0], template) )
+plt.imshow( mask_to_full(roi_2, np.where(template.flatten() == 1)[0], template) )
+
+roi = roi_0+roi_1+roi_2
+plt.imshow( mask_to_full(roi, np.where(template.flatten() == 1)[0], template) )
