8cf511b93b2a08684af9a85670edd8e5c0873297
JYKIM7 <71793706+JYKIM7@users.noreply.github.com>
2023-02-01 07:51:36 -0500

    Update run_script_v2.py

--- Run_1/run_script_v2.py
+++ Run_1/run_script_v2.py
@@ -2,7 +2,6 @@
 Run main script: Dynamic Abnormality Detection and Progression (DADP) pipeline
 Usage: python ./DADP_run_script.py ./data/ ./result/
 
-
 Author: Chao Huang (chaohuang.stat@gmail.com)
 """
 
